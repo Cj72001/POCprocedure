945398b1755942e5a5500f0795c1a016d386c6b4
Omar Flores <00038619@uca.edu.sv>
2024-01-02 17:02:28 -0600

    Add: screen_roles, categories inserts
    Add:
    -screen_roles (con las nuevas screens 54, 55 y 56)
    -categories (agregar las nuevas categorias fijas para cada taller)

--- create_workshopREGISTERS_VALIDATION.sql
+++ create_workshopREGISTERS_VALIDATION.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS CATEGORIES (
+    category_id SERIAL PRIMARY KEY,
+    category_name VARCHAR(255),
+    workshop_id INT,
+    category_active INT,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
 select * from BRANDS;
 select * from INVOICE_TYPES;
 select * from MODELS;
@@ -5,8 +14,8 @@ select * from PACKAGE_PROCESS;
 select * from PACKAGES;
 select * from PROCESS;
 select * from SCREEN_ROLES;
-select * from SCREENS;
 select * from SERIES;
 select * from SERVICE_TYPES;
 select * from VEHICLE_TYPES;
+select * from CATEGORIES;
 
